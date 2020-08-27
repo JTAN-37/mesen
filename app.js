fdb27b11c95da98152c7fd6a64eb8f6c13563e76
Jeremy Tan <jasonwuwho52@gmail.com>
2020-08-27 00:36:39 -0500

    added back end

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set('view-engine', 'ejs');
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -22,23 +25,22 @@ app.post("/", function(req, res) {
 
     response.on("data", function(dataChunk) {
       rawData += dataChunk;
-
     });
 
     response.on('end', function() {
       const newsData = JSON.parse(rawData);
-      const article = newsData.articles[0];
+      var articles = [];
 
-      const articleAuthor = article.author;
-      const articleTitle = article.title;
-      const articleDesc = article.description;
+      for (var i = 0; i < 6; i++) {
+        articles.push(newsData.articles[i]);
+      }
 
-      res.set("Content-Type", "text/html");
+      /*res.set("Content-Type", "text/html");
 
       res.write('<h2>' + articleAuthor + '</h2>');
       res.write('<h2>' + articleTitle + '</h2>');
-      res.write('<h2>' + articleDesc + '</h2>');
-      res.send();
+      res.write('<h2>' + articleDesc + '</h2>'); */
+      res.render('results.ejs', {articles: articles});
     });
 
   });
